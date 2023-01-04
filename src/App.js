f13d38033b446d6c1c7ac7a829dc4dae7d298e00
Pau <pauligonzalezincker@gmail.com>
2023-01-03 22:22:17 -0500

    built nav header and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { Route, Routes } from 'react-router';
-import Nav from './components/Home/Nav.js';
+// import { Route, Routes } from 'react-router';
+import './App.css';
+import Footer from './components/CommonComponents/Footer.js';
+import Nav from './components/CommonComponents/Nav.js';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Nav />}/>
-    </Routes>
+    <>
+    <Nav/>
+    {/* // <Routes>
+    //   <Route path='/' element={<Nav/>}/>
+    // </Routes> */}
+    <Footer/>
+    </>
   );
 }
 
---
+++ src/components/CommonComponents/Nav.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo_black.png"
+
+const Nav = () => {
+    return (
+        <header>
+            <nav>
+                <Link to="/">
+                    <div className="logoContainer">
+                        <img src={logo} alt="logo"/>
+                    </div>
+                </Link>
+                <ul>   
+                    <Link to="/the-opportunity"><li>The opportunity</li></Link>
+                    <Link to="/call-to-action"><li>Call To Action</li></Link>
+                    <Link to="/newsroom"><li>Newsroom</li></Link>
+                </ul>
+            </nav>
+        </header>
+    )
+};
+
+export default Nav;
