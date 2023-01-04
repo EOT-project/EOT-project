543d9aec8dfadf04d8033552264a142149b4b7f4
Jaehyun(Jay) Shim <jaehyunshim85@gmail.com>
2023-01-04 10:59:52 -0700

    intergrated data from Contentful

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import useContentful from './useContentful';
+
 
 function App() {
+
+  const [members, setMembers] = useState([]);
+  const { getMembers } = useContentful();
+
+  useEffect(() => {
+    getMembers().then((res) => console.log(res));
+  })
+
   return (
     <div className="App">
-      <h1>hola</h1>
+      <h1>working?</h1>
     </div>
   );
 }
---
+++ src/useContentful.js
@@ -0,0 +1,25 @@
+import { createClient } from "contentful";
+import * as contentful from 'contentful';
+
+const useContentful = () => {
+  const client = contentful.createClient({
+    space: process.env.REACT_APP_SPACE_ID,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+    host: "cdn.contentful.com"
+  });
+
+  const getMembers = async () => {
+    try {
+      const entries = await client.getEntries({
+        // content_type: "steeringCommittee"
+      });
+      return entries
+    } catch (error) {
+      console.log(`Error fetching members: ${error}`);
+    }
+  };
+
+  return {getMembers};
+};
+
+export default useContentful;
