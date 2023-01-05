aa74680fb81d93ea58d4eea292313dab7c027588
Jaehyun(Jay) Shim <jaehyunshim85@gmail.com>
2023-01-05 11:14:47 -0700

    created data cleanup fxn

--- src/components/Committee.js
+++ src/components/Committee.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import useContentful from "../useContentful";
 
 
@@ -7,21 +7,50 @@ const Committee = () => {
   const [members, setMembers] = useState([]);
   const { client } = useContentful();
 
-  const getMembers = async () => {
-    try {
-      const res = await client.getEntries({
-        content_type: "steeringCommittee"
-      })
-      setMembers(res.items)
-    } catch (error) {
-      console.log(`Error fetching members: ${error}`);
-    }
-  }
+  // const cleanUpData = useCallback((rawData) => {
+  //   const cleanData = rawData.map((data) => {
+  //     const { sys, fields } = data
+  //     const { id } = sys
+  //     const dataTitle = fields.title
+  //     const dataName = fields.name
+  //     const updatedData = {id, dataTitle, dataName}
+  //     return updatedData
+  //   })
+
+  //   setMembers(cleanData)
+  // }, [])
   
   useEffect(() => {
+    const getMembers = async () => {
+      try {
+        const res = await client.getEntries({
+          content_type: "steeringCommittee"
+        })
+        if (!!res) {
+          // setMembers(res.items)
+          // cleanUpData(res.items)
+          const cleanUpData = (rawData) => {
+            const cleanData = rawData.map((data) => {
+              const { sys, fields } = data
+              const { id } = sys
+              const dataTitle = fields.title
+              const dataName = fields.name
+              const updatedData = {id, dataTitle, dataName}
+              return updatedData
+            })
+            setMembers(cleanData)
+          }
+          cleanUpData(res.items)
+        } else {
+          setMembers([])
+        }
+      } catch (error) {
+        console.log(`Error fetching members: ${error}`);
+      }
+    }
     
     getMembers();
-    
+
   }, []);
   console.log(members);
   
@@ -33,7 +62,7 @@ const Committee = () => {
     <div>
       <h3>Steering Committee</h3>
       {
-        members.map((member)=><p>{member.fields.name}</p>)
+        members.map((member)=><p>{member.dataName}</p>)
       }
     </div>
   )
