ecb96ac7dc90cc6236f521291486b51a60be018f
Pau <pauligonzalezincker@gmail.com>
2023-01-11 16:38:07 -0500

    fixed wrapper, margins and width

--- src/UI/MainCard.js
+++ src/UI/MainCard.js
@@ -1,7 +1,7 @@
 const MainCard = (props) => {   
      
     return (
-        <div className="mainCard" style={{"background": `linear-gradient( 90deg,rgb(211, 200, 224) 50%, transparent, rgb(255, 255, 255) 100%), url(${props.image})`, "backgroundRepeat": "no-repeat", "backgroundPosition": "center",
+        <div className="mainCard" style={{"background": `linear-gradient(90deg, #D5C7E2 1%, rgba(213,199,226,0.90) 62%, rgba(255,255,255,0.00) 100%), url(${props.image})`, "backgroundRepeat": "no-repeat", "backgroundPosition": "center",
         "backgroundSize": "cover"}}>{props.children}</div>
     );
 }
