db2dd5635f999de14e0783762b235056a014a25f
Pau <pauligonzalezincker@gmail.com>
2023-01-13 12:09:57 -0500

    testing readme file

--- src/UI/MainCard.js
+++ src/UI/MainCard.js
@@ -1,7 +1,7 @@
 const MainCard = (props) => {   
      
     return (
-        <div className="mainCard" style={{"background": `linear-gradient(90deg, #D5C7E2 1%, rgba(213,199,226,0.90) 62%, rgba(255,255,255,0.00) 100%), url(${props.image})`, "backgroundRepeat": "no-repeat", "backgroundPosition": "center",
+        <div className="mainCard" style={{"background": `linear-gradient(90deg, #BAE6DA 1%, rgba(213,199,226,0.90) 62%, rgba(255,255,255,0.00) 100%), url(${props.image})`, "backgroundRepeat": "no-repeat", "backgroundPosition": "center",
         "backgroundSize": "cover"}}>{props.children}</div>
     );
 }
