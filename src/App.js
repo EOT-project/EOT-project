f4c2d84e3803ee89dc5f8772f3acfe2c67aa103f
Jaehyun(Jay) Shim <jaehyunshim85@gmail.com>
2023-01-20 11:05:42 -0700

    removed un-used components and its styling files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { Route, Routes } from 'react-router';
-import './App.css';
-import Footer from './components/Footer.js';
 import Nav from './components/Nav.js';
+import Footer from './components/Footer.js';
 import Home from "./pages/Home";
 import Opportunity from './pages/Opportunity';
 import CallToAction from './pages/CallToAction';
 import NewsRoom from './pages/Newsroom';
 import Resources from './pages/Resources';
-import Blog from './pages/Blog';
 import ErrorPage from './pages/ErrorPage';
 
 function App() {
@@ -21,7 +19,6 @@ function App() {
         <Route path='/action'element={<CallToAction/>}/>
         <Route path='/newsroom' element={<NewsRoom/>} />
         <Route path='/resources' element={<Resources/>}/>
-        <Route path='/blog' element={<Blog/>}/>
         <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Footer/>
--- src/UI/Loader.js
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-    return (
-        <div className='loadingContainer'>
-            <div className='ldsRoller'><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-        </div>
-    );
-}
-
-export default Loader;
