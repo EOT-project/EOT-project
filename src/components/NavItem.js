825a2407105bfe36a5ad7ee3b2cc8a9abda10d67
Jaehyun(Jay) Shim <jaehyunshim85@gmail.com>
2023-01-20 04:20:02 -0700

    added whoWeAre link on nav that links to homePage

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 const NavItem = (props) => {
 
     return (
-        <li className="loading"><Link to={props.value} className="loading"><p className="loading">{props.name}</p></Link></li>
+        !props.value
+        ?
+            <li className="loading"><Link to="/" className="loading"><p className="loading">{props.name}</p></Link></li>
+        :
+            <li className="loading"><Link to={props.value} className="loading"><p className="loading">{props.name}</p></Link></li>
     );
 };
 
