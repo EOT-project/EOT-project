5541362237a3b403577928ddf72a66cac6f2d520
Jaehyun(Jay) Shim <jaehyunshim85@gmail.com>
2023-01-22 14:34:36 -0700

    added one more body content on opportunity page as per client's request

--- src/pages/Opportunity.js
+++ src/pages/Opportunity.js
@@ -9,10 +9,11 @@ const Opportunity = () => {
         <main>
             <Articles />
             <section className="factsStatements wrapper">
-                <InfographicWithContent/>
                 <ContentOpportunity order={1}/>
-                <InfographicOnly order={1}/>
+                <InfographicWithContent/>
                 <ContentOpportunity order={2}/>
+                <InfographicOnly order={1}/>
+                <ContentOpportunity order={3}/>
                 <InfographicOnly  order={2}/>
             </section>
         </main>
