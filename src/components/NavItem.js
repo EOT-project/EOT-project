5c262ebff779c08564b22ea75ec4297938dcaba6
Pau <pauligonzalezincker@gmail.com>
2023-01-23 15:09:36 -0500

    fixed nav on landscape and added some style

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const NavItem = (props) => {
 
     const goToTop = () => {
         window.scrollTo({
             top: 0,
-            behavior: "smooth",
+            behavior: "instant",
         });
     };
 
